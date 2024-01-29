487fc8a9ea87a734819d3b192c516b9a98b9bbeb
Just-andreew <andrewsolitei@gmail.com>
2024-01-29 15:34:47 +0300

    control-flow

---
+++ jspractice.js
@@ -0,0 +1,22 @@
+//Ternary operators
+/*
+let subject = 65;
+let result = subject > 45 ? 'Passed':'Failed';
+console.log(result); 
+*/
+
+// Switch  Case Example
+let today = "Sunday"
+switch (today) {
+
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursay":
+    case "Friday": 
+    console.log("Today's a working day")
+    break;
+
+    default:
+        console.log("It is a Weekend");
+}
