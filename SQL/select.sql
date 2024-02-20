8acd11b3517cb54267e83453653232e86be84c89
Just-andreew <andrewsolitei@gmail.com>
2024-02-21 01:50:56 +0300

    WHERE

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,1 +1,4 @@
-SELECT 
+SELECT name,
+unit_price AS 'Price'
+unit_price * 1.1 AS  'Price with Tax'
+FROM products;
---
+++ SQL/where.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM orders
+WHERE order_date BETWEEN '2018-07-08' AND '2019-06-25';
